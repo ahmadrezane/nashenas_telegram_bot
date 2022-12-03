c2e1aebf04175299cb3fbfab5f0b759829ae30b5
ahmadreza nezamdoust <ahmadrezanee@gmail.com>
2022-12-03 16:27:34 +0330

    add base code for nashenas bot

--- src/constant.py
+++ src/constant.py
@@ -2,18 +2,30 @@ from types import SimpleNamespace
 from telebot import types
 import emoji
 
-def keyboard(*keys, row_width=2, resize_keyboard=True):
-   markup = types.ReplyKeyboardMarkup(row_width=row_width, resize_keyboard=resize_keyboard)
+def keyboard (*keys, row_width=2, resize_keyboard=True):
+   markup = types.ReplyKeyboardMarkup(
+    row_width=row_width,
+    resize_keyboard=resize_keyboard
+    )
    button = map(types.KeyboardButton, keys)
    markup.add(*button)
    return markup
 
 keys = SimpleNamespace(
-    main='main',
-    hi='hi:waving_hand_light_skin_tone:'
+    random_connect='Random connect:busts_in_silhouette:',
+    setting='Setting:gear:',
+    stop='Stop connecting:red_circle:'
 )
 
 mykeyboard = SimpleNamespace(
-    first=keyboard(keys.main, emoji.emojize(keys.hi))
-    )
+    main=keyboard(emoji.emojize(keys.random_connect),
+    emoji.emojize(keys.setting)),
+    second=keyboard(emoji.emojize(keys.stop))
+)
 
+states = SimpleNamespace(
+    idle='IDLE',
+    random_connect='RANDOM_CONNECT',
+    stop='STOP',
+    connected='CONNECTED'
+)
--- src/run.py
+++ src/run.py
@@ -1,11 +1,13 @@
 import emoji
+
 from loguru import logger
 
 from src.bot import bot
-from src.constant import mykeyboard
+from src.constant import mykeyboard, keys, states
+from src.db import db
 from src.filters import IsAdmin
-from src.utils.io import read_json, write_json
 
+# flake8: noqa on a line by itself
 
 class Bot:
    def __init__(self, telebot):
@@ -22,22 +24,122 @@ class Bot:
       self.bot.infinity_polling()
 
    def handlers(self):
+      @self.bot.message_handler(commands=['start', 'help'])
+      def start(message):
+         self.bot.reply_to(message, 'Hi how can i help you', reply_markup=mykeyboard.main)
+         db.users.update_one(
+            {'chat_id': message.chat.id},
+            {'$set': message.json},
+            upsert=True
+            )
+         self.update_state(message.chat.id, states.idle)
+
+      @self.bot.message_handler(regexp=emoji.emojize(keys.random_connect))
+      def random_connect(message):
+         self.bot.send_message(
+            message.chat.id,
+            '<strong>Connecting to a random stranger...</strong>',
+            reply_markup=mykeyboard.second
+            )
+         self.update_state(message.chat.id, states.random_connect)
+         other_user = db.users.find_one(
+            {
+               'state': states.random_connect,
+               'chat_id': {'$ne': message.chat.id}
+            }
+         )
+
+         if not other_user:
+            return
+         #update other_user state
+         self.update_state(other_user['chat']['id'], states.connected)
+         self.bot.send_message(
+            other_user['chat']['id'],
+            f"you are connected to {message.chat.first_name} ..."
+            )
+
+         #update user state
+         self.update_state(message.chat.id, states.connected)
+         self.bot.send_message(
+            message.chat.id,
+          f'you are connected to {other_user["chat"]["first_name"]} ....'
+          )
+
+         #store connected user
+         db.users.update_one(
+            {'chat_id': message.chat.id},
+            {'$set': {'connected_to':other_user['chat']['id']}}
+         )
+
+         db.users.update_one(
+            {'chat_id': other_user['chat']['id']},
+            {'$set': {'connected_to':message.chat.id}}
+         )
+
+      @self.bot.message_handler(regexp=emoji.emojize(keys.stop))
+      def stop(message):
+         self.bot.send_message(
+            message.chat.id,
+             'stop connection',
+              reply_markup=mykeyboard.main
+              )
+         self.update_state(message.chat.id, states.stop)
+
+         # update other user state
+         connected_to = db.users.find_one(
+            {'chat_id':message.chat.id}
+         )['connected_to']
+
+         if not connected_to:
+            return
+         #disconnect users
+         self.bot.send_message(connected_to, f'{message.chat.first_name} leave the chat')
+
+         self.update_state(connected_to, states.stop)
+
+         # remove connected users
+         db.users.update_one(
+            {'chat_id': message.chat.id},
+            {'$set': {'connected_to': None }}
+         )
+
+         db.users.update_one(
+            {'chat_id':connected_to},
+            {'$set': {'connected_to': None}}
+         )
+
 
       @self.bot.message_handler(is_chat_admin=True)
       def is_admin(message):
          self.bot.send_message(message.chat.id, 'oh you are the boss and admin of this group')
+
       @self.bot.message_handler(func=lambda message:True)
       def echo_all(message):
-            self.send_message(message.chat.id,
-             message.text,
-              reply_markup=mykeyboard.first
-              )
-            print(emoji.demojize(message.text))
+         """"
+         echo message to other connected user
+         """
+         
+            user = db.users.find_one(
+               {'chat_id': message.chat.id}
+            )
+            if (not user) (user['state'] != states.connected) or (user['connected_to'] == None):
+               return
+
+            self.bot.send_message(user['connected_to'], message.text)
+
    def send_message(self, chat_id, text, reply_markup=None, emojize=True):
       if emojize:
          text = emoji.emojize(text)
       self.bot.send_message(chat_id, text, reply_markup=reply_markup)
 
+   def update_state(self, chat_id, state):
+      """
+      update user state
+      """
+      db.users.update_one(
+         {'chat_id': chat_id},
+         {'$set': {'state': state}}
+      )
 
 
 
@@ -45,5 +147,5 @@ class Bot:
 
 if __name__ == '__main__':
    bot = Bot(telebot=bot)
-   bot.run()
+
 
